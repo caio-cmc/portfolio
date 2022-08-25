c2e5810d1f8e2e36b4288cbcf9c3fd978b7207b3
caio-cmc <caiocesarmcruz@gmail.com>
2022-08-25 19:09:15 -0300

    feat(Pages): :art:

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Components/Header';
+import personExample from '../Images/personExample.png'
 
 function About() {
   return (
     <div>
       <Header />
-      <h1>About</h1>
+      <img src={ personExample } alt="eu" />
+      <p>Alguma coisa sobre mim aqui</p>
     </div>
   );
 }
--- src/Pages/Stacks.jsx
+++ src/Pages/Stacks.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../Components/Header';
+import StacksInfo from '../MockFiles/Stacks';
 
 function Stacks() {
   return (
     <div>
       <Header />
       <h1>Stacks</h1>
+      {StacksInfo.map((stack) => (
+        <div>
+          <h2>{ stack.name }</h2>
+          <img src={ stack.image } alt={ `${stack.name} logo` } />
+        </div>
+      ))}
     </div>
   );
 }
