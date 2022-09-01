ce5dd8b6c15842816c4ad078c0a3fc2eee6b1f57
caio-cmc <caiocesarmcruz@gmail.com>
2022-09-01 13:20:30 -0300

    style(Homepage): :lipstick: Style added to homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from '../Components/Header';
 import eu from '../Images/eu.jpeg'
+import '../Styles/Pages/Homepage.scss'
 
 function Homepage() {
   return (
     <div>
       <Header />
-      <img src={ eu } alt="Eu" />
-      <p>Bem vindos ao meu portfólio, meu nome é Caio César e eu serei seu guia!</p>
+      <div className="homepage-main">
+        <img src={ eu } alt="Caio profile" className="homepage-caio-profile" />
+        <p className="homepage-intro">
+          Eaí pessoal! Sejam bem vindos ao meu portfólio, meu nome é Caio César, desenvolvedor fullstack e eu serei seu guia!
+        </p>
+      </div>
     </div>
   );
 }
