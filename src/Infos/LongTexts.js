a7adb1caaed7786a5a3c784e0bcf39f2c38fa647
caio-cmc <caiocesarmcruz@gmail.com>
2023-02-13 13:11:08 -0300

    contact page texts updated

--- src/Infos/LongTexts.js
+++ src/Infos/LongTexts.js
@@ -10,11 +10,29 @@ const projPT = "Por aqui mostrarei alguns dos meus projetos já desenvolvidos na
 
 const projEN = "English text, the book is on the table, yadayadayada e essas coisas assim";
 
+const contPT1 = "Gostaram do que viram? Entrem em contato comigo!";
+
+const contEN1 = "Do you like what you see? Contact me!";
+
+const contPT2 = "Preenchendo esse formulário aqui embaixo um e-mail será enviado para mim, vou responder o mais rápido o possível, eu prometo.";
+
+const contEN2 = "By filling in this form down here an e-mail will be sent to me, I'll reply as soon as possible, I promise.";
+
+const contPT3 = "No final da página tem os links pras minhas redes sociais, sinta-se livre para dar uma checada :)";
+
+const contEN3 = "At the bottom of the page there's my social media links, feel free to check them out :)";
+
 export {
   homePT1,
   homeEN1,
   homePT2,
   homeEN2,
   projPT,
-  projEN
+  projEN,
+  contPT1,
+  contEN1,
+  contPT2,
+  contEN2,
+  contPT3,
+  contEN3,
 };
