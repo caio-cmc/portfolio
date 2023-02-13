36b774689d2599ec0f800ebdb9d345ecc0e8dd13
caio-cmc <caiocesarmcruz@gmail.com>
2023-02-13 11:37:24 -0300

    homepage texts updated

--- src/Infos/LongTexts.js
+++ src/Infos/LongTexts.js
@@ -1,14 +1,20 @@
-const homePT = "Eaí pessoal! Sejam bem vindos ao meu portfólio, meu nome é Caio César, desenvolvedor fullstack e eu serei seu guia!";
+const homePT1 = "Eaí pessoal, meu nome é";
 
-const homeEN = "English text, the book is on the table, yadayadayada e essas coisas assim";
+const homeEN1 = "Hey Guys, my name is";
+
+const homePT2 = "sejam bem-vindos ao meu portfólio, eu serei seu guia!";
+
+const homeEN2 = "welcome to my portfolio, I'll be your guide!";
 
 const projPT = "Por aqui mostrarei alguns dos meus projetos já desenvolvidos na minha jornada como desenvolvedor";
 
 const projEN = "English text, the book is on the table, yadayadayada e essas coisas assim";
 
 export {
-  homePT,
-  homeEN,
+  homePT1,
+  homeEN1,
+  homePT2,
+  homeEN2,
   projPT,
   projEN
 };
